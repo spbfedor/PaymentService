5c7fc59ab9d5d54a44838a0e2dbab89fe685f343
spbfedor <spbfedor@yandex.ru>
2026-02-13 18:57:37 +0300

    Завершены настройки БД

---
+++ app/core/config.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy import URL 
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+ENV_PATH = os.path.join(BASE_DIR, ".env")
+
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    PROJECT_NAME: str = "PaymentService"
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_PATH,
+        env_file_encoding="utf8",
+        extra="ignore"
+    )
+
+    @property
+    def database_url(self) -> URL:
+        return URL.create(
+            drivername="postgresql+asyncpg",
+            username=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            database=self.DB_NAME,
+        )
+
+
+settings = Settings()
---
+++ app/db/session.py
@@ -0,0 +1,20 @@
+from core.config import settings
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine
+)
+from sqlalchemy.orm import DeclarativeBase
+
+
+engine = create_async_engine(settings.database_url, echo=True)
+async_session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+
+class Base(DeclarativeBase):
+    pass
