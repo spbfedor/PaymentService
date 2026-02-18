e2f763d404ddf6b711f6e362e32d5c23542f3721
spbfedor <spbfedor@yandex.ru>
2026-02-18 21:37:16 +0300

    Проект завершен

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from sqlalchemy import URL 
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
